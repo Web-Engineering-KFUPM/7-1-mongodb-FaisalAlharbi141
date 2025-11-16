741ace4abeda0d32c3a07a6e4863114e0fe4efd8
FaisalAlharbi141 <fash.a.1818@gmail.com>
2025-11-16 12:12:10 +0300

    Finished Todo 1& 2 of mongoose

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -172,10 +172,22 @@ import mongoose from "mongoose";
 // establish connection
 
 // define schema
-
-
+const studentSchema = new mongoose.Schema({
+         name: String,
+         age: Number,
+         major: String
+      });
+      
+      const Student = mongoose.model("Student", studentSchema);
 // create document
-
+async function createStudents() {
+      await Student.insertMany([
+         { name: "Ali", age: 21, major: "CS" },
+         { name: "Sara", age: 23, major: "SE" }
+      ]);
+      console.log("✅ Inserted");
+      }
+      createStudents();
 
 // read document
 
